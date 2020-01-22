623e3d1e14db202887b65183dcd14628aff1d703
RunaNam <nmj5012@naver.com>
2020-01-22 16:27:42 +0900

    Part6

--- polls/models.py
+++ polls/models.py
@@ -12,7 +12,7 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return self.pub_date>= timezone.now() - datetime.timedelta(days=1)
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -22,3 +22,8 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+
+def was_published_recently(self):
+    now = timezone.now()
+    return now - datetime.timedelta(days=1) <= self.pub_date <= now
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
+from django.utils import timezone
 from django.views import generic
 
 from .models import Question, Choice
@@ -11,12 +12,17 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pubdate')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).oder_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultView(generic.DetailView):
