663eecddb5c4ce0d9fb12ac225b8da488f650177
RunaNam <nmj5012@naver.com>
2020-01-22 22:56:36 +0900

    part6

--- polls/views.py
+++ polls/views.py
@@ -14,7 +14,7 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Question.objects.filter(
             pub_date__lte=timezone.now()
-        ).oder_by('-pub_date')[:5]
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
